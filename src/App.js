32b169dc0629a3b6dfeb62054bfc5a92a159ad9c
Sam Tarullo <sam@Sams-Air.attlocal.net>
2020-09-09 22:30:59 -0500

    changed a couple of thangzz

--- src/App.js
+++ src/App.js
@@ -11,28 +11,47 @@ function App() {
   const [exp, setExp] = useState('');
   const [date, setDate] = useState('');
   const [inputValue, setInputValue] = useState('');
-  useEffect(() => {
+  useEffect(()=>{
 axios.get('https://api.nasa.gov/planetary/apod?api_key=YbUynbWVo92eAUAdlerdxb0JPthz9HDt5YPvb9PW')
 .then(res=>{
+  console.log(res)
   setUrl(res.data.url)
   setTitle(res.data.title);
   setExp(res.data.explanation);
   setDate(res.data.date)
 })
+.catch(err=>{
+  let day = prompt('Sorry, there is no photo of the day for today yet. Come back later or enter a specific date in the format "2019-02-20"');
+  axios.get(`https://api.nasa.gov/planetary/apod?api_key=YbUynbWVo92eAUAdlerdxb0JPthz9HDt5YPvb9PW&date=${day}`)
+  .then(res=>{
+    setUrl(res.data.url);
+    setTitle(res.data.title);
+    setExp(res.data.explanation);
+    setDate(res.data.date);
+  })
+  .catch(err=>{
+    alert("You either need to pick a different date or use the right format")
+  })
+});
   }, []);
+
 
   const changeInput = obj => {
 const value = obj.target.value;
 setInputValue(value);
   };
-  
+
   const getRandUrl = () => {
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=YbUynbWVo92eAUAdlerdxb0JPthz9HDt5YPvb9PW&date=${inputValue}`)
     .then(res=>{
+      console.log(res)
       setUrl(res.data.url)
       setTitle(res.data.title);
       setExp(res.data.explanation);
       setDate(res.data.date)
+    })
+    .catch(err=>{
+      alert('Please enter a different date or use the correct format!')
     })
   }
 
